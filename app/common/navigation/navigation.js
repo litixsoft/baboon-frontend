38024076349f33cc30a7f42102390b6752f94af5
Sven Bernstein <s.bernstein@litixsoft.de>
2014-07-01 12:28:23 +0200

    chore: refactoring navigation code much more slim

--- app/common/navigation/navigation.js
+++ app/common/navigation/navigation.js
@@ -6,7 +6,6 @@ angular.module('common.navigation',[])
             [
                 {
                     'title': 'Main',
-                    'norouting': true,
                     'app': 'main',
                     'children': [
                         {
@@ -55,198 +54,21 @@ angular.module('common.navigation',[])
                 'route': '/main/contact',
                 'app': 'main'
             }
+        ],
+        toplevel:[
+            {
+                'title': 'Home',
+                'route': '/main/home',
+                'app': 'main'
+            },
+            {
+                'title': 'Admin',
+                'route': '/admin/home',
+                'app': 'admin'
+            }
         ]
     })
-    .provider('$comNavProvider', function () {
-        var app;
-        var route;
-        var navList;
-        var navTree;
-
-        var addNavTreeObj = function (value) {
-
-            var navObj = {
-                title: value.title,
-                route: value.route,
-                controller: value.controller,
-                norouting: value.norouting || false,
-                app: value.app
-            };
-
-            if (value.level === 0 && value.order) {
-                navObj.order = value.order;
-            }
-
-            navObj.level = value.level;
-
-            if (value.target) {
-                navObj.target = value.target;
-            }
-
-            if (value.children) {
-                navObj.children = [];
-            }
-
-            return navObj;
-        };
-
-        var addNavListObj = function (value) {
-
-            var navObj = {
-                title: value.title,
-                route: value.route,
-                controller: value.controller,
-                norouting: value.norouting || false,
-                app: value.app
-            };
-
-            if (value.level === 0 && value.order) {
-                navObj.order = value.order;
-            }
-
-            navObj.level = value.level;
-
-            if (value.target) {
-                navObj.target = value.target;
-            }
-
-            return navObj;
-        };
-
-        var checkTreeNav = function (tmpNav, navTree, current, level, user) {
-
-            level += 1;
-
-            var i, max;
-
-            for (i = 0, max = tmpNav.length; i < max; i += 1) {
-
-//                var hasRight = rights.userHasAccessToController(user, tmpNav[i].controller);
-                var hasRight = true;
-
-                if (hasRight) {
-
-                    // add deep level of tree
-                    tmpNav[i].level = level;
-
-                    // check is current app
-                    if (tmpNav[i].app !== current) {
-
-                        // when not current set target to _self
-                        tmpNav[i].target = '_self';
-                    }
-
-                    if (i > 0 && !navTree[i - 1]) {
-                        navTree[i -1] = addNavTreeObj(tmpNav[i]);
-
-                        if (tmpNav[i].children) {
-                            checkTreeNav(tmpNav[i].children, navTree[i -1].children, current, level, user);
-                        }
-                    }
-                    else {
-                        navTree[i] = addNavTreeObj(tmpNav[i]);
-
-                        if (tmpNav[i].children) {
-                            checkTreeNav(tmpNav[i].children, navTree[i].children, current, level, user);
-                        }
-                    }
-                }
-            }
-        };
-
-        var checkListNav = function (tmpNav, current, level, user) {
-
-            level += 1;
-
-//            lxHelpers.forEach(tmpNav, function (value) {
-            angular.forEach(tmpNav, function (value) {
-
-//                var hasRight = rights.userHasAccessToController(user, value.controller);
-                var hasRight = true;
-
-                if (hasRight) {
-
-                    // add deep level of tree
-                    value.level = level;
-
-                    // check is current app
-                    if (value.app !== current) {
-
-                        // when not current set target to _self
-                        value.target = '_self';
-                    }
-
-                    // push in flat list
-                    navList.push(addNavListObj(value));
-
-                    // when children call recursive function
-                    if (value.children) {
-                        checkListNav(value.children, current, level, user);
-                    }
-                }
-            });
-        };
-
-
-        this.set = function (options) {
-            options = options || {};
-
-            app = options.app;
-            route = options.route;
-        };
-
-        this.$get = function () {
-            var pub = {};
-
-            pub.getTree = function (list, data) {
-                var tmpNav = list || [];
-                var current = data.current || 'main';
-                navTree = [];
-
-                checkTreeNav(tmpNav, navTree, current, -1, {});
-
-                return navTree;
-            };
-
-            pub.getList = function (list, data) {
-                var tmpNav = list || [];
-                var current = data.current || 'main';
-                navList = [];
-
-//                checkListNav(tmpNav, current, -1, request.session.user);
-                checkListNav(tmpNav, current, -1, {});
-
-                return navList;
-            };
-
-            pub.getTopList = function (list, data) {
-                var tmpNav = list || [];
-                var current = data.current || 'main';
-                navList = [];
-
-
-                angular.forEach(tmpNav, function (value) {
-                    var hasRight = true;//rights.userHasAccessToController(request.session.user, value.controller);
-
-                    if (hasRight) {
-
-                        value.level = 0;
-
-                        if (value.app !== current) {
-                            value.target = '_self';
-                        }
-
-                        navList.push(addNavListObj(value));
-                    }
-                });
-
-                return navList;
-            };
-
-            return pub;
-        };
-    })
-    .directive('comNav', function($location, $comNavProvider, ACTIVE_APP, NAVOBJ) {
+    .directive('comNav', function($location, ACTIVE_APP, NAVOBJ) {
         return {
             restrict: 'E',
             replace: true,
@@ -261,20 +83,12 @@ angular.module('common.navigation',[])
                 orientation: '@',
                 navLinklist: '@'
             },
-            link: function (scope, element, attrs) {
+            link: function (scope, element) {
 
-                var defaults = {
-                    top: { fn: 'getTopList', orientation: 'horizontal' },
-//                    sub: { fn: 'getSubList', orientation: 'vertical' },
-                    list: { fn: 'getList', orientation: 'vertical' }
-                };
-
-                var orientation = scope.orientation || defaults[attrs.type].orientation;
+                var orientation = scope.orientation || 'horizontal';
                 element.toggleClass('nav-stacked', orientation === 'vertical');
 
-                var fn = defaults[attrs.type].fn;
-                var linkList = NAVOBJ[scope.navLinklist || 'standard'];
-                scope.menu = $comNavProvider[fn](linkList,{current: scope.app});
+                scope.menu = NAVOBJ[scope.navLinklist || 'standard'];
 
                 scope.isActiveApp = function (app) {
                     return app === ACTIVE_APP;
@@ -286,7 +100,7 @@ angular.module('common.navigation',[])
             }
         };
     })
-    .directive('comNavTree', function($comNavProvider, $location, $templateCache, ACTIVE_APP, NAVOBJ) {
+    .directive('comNavTree', function($location, $templateCache, ACTIVE_APP, NAVOBJ) {
         return {
             restrict: 'E',
             replace: true,
@@ -302,9 +116,9 @@ angular.module('common.navigation',[])
                         '<div class="list-item" ng-class="{active: isActive(data.route)}">'+
                             '<div class="opensub {{data.hide}}" ng-show="data.children" ng-click="toggleShow(data)"></div>'+
                             '<div class="nav-icon {{data.icon}}"></div>'+
-                            '<a ng-if="!data.norouting && isActiveApp(data.app)" ng-class="{spacer: data.children.length > 0}" ng-href="{{data.route}}"><span>{{data.title}}</span></a>'+
-                            '<a ng-if="!data.norouting && !isActiveApp(data.app)" ng-class="{spacer: data.children.length > 0}" ng-href="{{data.route}}" target="_self"><span>{{data.title}}</span></a>'+
-                            '<a ng-if="data.norouting" ng-class="{spacer: data.children.length > 0}" ng-click="toggleShow(data);" style="cursor:pointer;"><span>{{data.title}}</span></a>'+
+                            '<a ng-if="data.route && isActiveApp(data.app)" ng-class="{spacer: data.children.length > 0}" ng-href="{{data.route}}"><span>{{data.title}}</span></a>'+
+                            '<a ng-if="data.route && !isActiveApp(data.app)" ng-class="{spacer: data.children.length > 0}" ng-href="{{data.route}}" target="_self"><span>{{data.title}}</span></a>'+
+                            '<a ng-if="!data.route" ng-class="{spacer: data.children.length > 0}" ng-click="toggleShow(data);" style="cursor:pointer;"><span>{{data.title}}</span></a>'+
                         '</div>'+
                         '<ul class="display {{data.hide}}" ng-if="data.children.length">'+
                             '<li ng-repeat="data in data.children" ng-include="\'bbc/navigation/tpls/treeview/inner.html\'"></li>'+
@@ -346,8 +160,8 @@ angular.module('common.navigation',[])
                     return app === ACTIVE_APP;
                 };
 
-                var linkList = NAVOBJ[scope.navLinklist || 'standard'];
-                scope.navList = $comNavProvider.getTree(linkList,{current: scope.app});
+
+                scope.navList = NAVOBJ[scope.navLinklist || 'standard'];
                 if (scope.navList.length !== 0) {
                     angular.forEach(scope.navList,function(value){
                         if(value.app===scope.app){
