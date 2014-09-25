09025af1b00dd85daa12da00c2efbdfb1a6830d4
Timo Liebetrau <t.liebetrau@litixsoft.de>
2014-09-25 14:38:35 +0200

    chore: optimize develop settings with no cache

--- server/app.js
+++ server/app.js
@@ -12,11 +12,11 @@ var cors = require('cors');
  *
  * @param options
  */
-module.exports = function(options) {
+module.exports = function (options) {
 
   var config = options.config;
 
-  if (! config) {
+  if (!config) {
     throw new Error('Parameter Error: config required');
   }
 
@@ -28,27 +28,36 @@ module.exports = function(options) {
   var RELOAD = process.env.RELOAD || null;
 
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(methodOverride());
   app.use(cors());
   app.use(logger('dev'));
 
   // development config
-  if ('development' === env ) {
+  if ('development' === env) {
 
     // Check livereload
     if (RELOAD) {
       app.use(require('connect-livereload')());
     }
 
+    // Disable caching of scripts for easier testing
+    app.use(function noCache(req, res, next) {
+
+      res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+      res.header('Pragma', 'no-cache');
+      res.header('Expires', 0);
+
+      next();
+    });
+
     app.use(favicon(config.ROOT_PATH + '/app/favicon.ico'));
     app.use(express.static(path.join(config.ROOT_PATH, '.tmp')));
     app.use(express.static(config.APP_PATH));
-    //app.use('/bower_components', express.static(path.join(config.ROOT_PATH, 'bower_components')));
   }
 
   // production config
-  if ('production' === env ) {
+  if ('production' === env) {
     app.use(compression());
     app.use(favicon(config.ROOT_PATH + '/build/dist/favicon.ico'));
     config.APP_PATH = path.join(config.ROOT_PATH, 'build', 'dist');
@@ -61,7 +70,7 @@ module.exports = function(options) {
     var app = req.params.app;
     var appFile = app + '.html';
 
-    if (appFile === 'main.html' || !fs.existsSync(path.join(config.APP_PATH, appFile ))) {
+    if (appFile === 'main.html' || !fs.existsSync(path.join(config.APP_PATH, appFile))) {
       res.sendfile('index.html', {root: config.APP_PATH});
     }
     else {
