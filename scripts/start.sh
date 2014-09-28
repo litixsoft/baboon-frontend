79f038f841e087b53c5a91b2641ee7b017db05e6
Timo Liebetrau <t.liebetrau@litixsoft.de>
2014-09-28 17:06:24 +0200

    chore: update start.sh and packages

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Script Path
+SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Go to root path of application
+cd $SCRIPT && cd ..
+
+# Set environment
+export DEBUG=baboon*
+export NODE_ENV=development
+export HOST=127.0.0.1
+export PORT=9000
+
+# Start application
+node ./server/bin/www.js
