28c3b55a0357e1b20571fa9514f90fc5cbd70405
Thomas Scheibe <th.scheibe@litixsoft.de>
2014-10-07 14:54:12 +0200

    chore: windows reset script (reset.cmd)

--- app/modules/admin/home/home.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('admin.home', [])
-  .config(function ($stateProvider) {
-    $stateProvider.state('adminHome', {
-      url: '/admin/home', templateUrl: '/modules/admin/home/home.html'
-    });
-  });
--- app/modules/examples/home/home.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('examples.home', [])
-  .config(function ($stateProvider) {
-    $stateProvider.state('examplesHome', {
-      url: '/examples/home', templateUrl: '/modules/examples/home/home.html'
-    });
-  });
